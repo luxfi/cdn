9d64ad1b745ea18241f747fcc6353c13aead2da1
Fuma Nama <fuma-nama@noreply.com>
2025-12-18 14:29:44 -0800

    Use static layout without server actions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,35 @@
-export const metadata = {
-  title: 'Lux CDN',
-  description: 'Lux Network content delivery network app',
+import React, { type PropsWithChildren } from 'react'
+import type { Viewport, Metadata } from 'next'
+
+import { Toaster } from '@hanzo/ui/primitives'
+import { Header } from '@luxfi/ui'
+
+import siteDef from '../site-def'
+import _metadata from '../metadata'
+
+import '@luxfi/ui/style/lux-global.css'
+
+export const metadata: Metadata = { ..._metadata }
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const bodyClasses = 'bg-background text-foreground flex flex-col min-h-full font-sans'
+
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en" style={{backgroundColor: 'black'}}>
-      <body>{children}</body>
+    <html lang='en' className='dark'>
+      <body className={bodyClasses}>
+        <Header siteDef={siteDef} />
+        <main className='flex flex-col grow'>
+          {children}
+        </main>
+        <Toaster />
+      </body>
     </html>
   )
 }
